a8b2e4513126fc54b84233ccd3f18fb5ddcb6344
Ryvqi <rifqikhabibi03@gmail.com>
2023-06-23 11:05:07 +0700

    cout

--- CircularQueus/CircularQueus.cpp
+++ CircularQueus/CircularQueus.cpp
@@ -83,7 +83,8 @@ public:
 		else {
 			//jika FRONT_position > REAR_position, iterasi dari FRONT hingga akhir array
 			while (FRONT_position <= n - 1) {
-
+				cout << queue_array[FRONT_position] << "  ";
+				FRONT_position++;
 			}
 		}
 	}
