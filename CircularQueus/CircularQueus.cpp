aaa62226fef020138d7ef855ddd821894729cbb1
Ryvqi <rifqikhabibi03@gmail.com>
2023-06-23 10:59:59 +0700

    cout

--- CircularQueus/CircularQueus.cpp
+++ CircularQueus/CircularQueus.cpp
@@ -69,5 +69,12 @@ public:
 			cout << "Queue is empty\n";
 			return;
 		}
+
+		cout << "\nElements is in the queue are...\n";
+
+		//jika FRONT_position <= REAR_position, iterasi dari FRONT hingga REAR
+		if (FRONT_position <= REAR_position) {
+
+		}
 	}
 };
